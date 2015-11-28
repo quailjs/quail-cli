5907bb9b7cd3996b656ca2734ec9260d2941e7ca
Jesse Renee Beach <splendidnoise@gmail.com>
2015-11-27 17:42:26 -0800

    Adding a file for the quailBuild command.

---
+++ src/quailBuild.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var config = require('./config');
+var cwd = process.cwd();
+var state = {};
+
+module.exports = function quailBuild(cmd) {
+
+};
