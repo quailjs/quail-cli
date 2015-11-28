0eb75514f003dd584a1804cfcb4c536c27cf5e8e
Jesse Renee Beach <splendidnoise@gmail.com>
2015-11-28 00:43:00 -0800

    Build a Quail dist

--- src/quailBuild.js
+++ src/quailBuild.js
@@ -4,17 +4,14 @@
 
 const fs = require('fs');
 const path = require('path');
-const config = require('./config');
+const configUtil = require('./config');
 const cwd = process.cwd();
 const quailCore = require(path.join(cwd, 'node_modules', '@quailjs/quail-core'));
 const state = {};
 
 module.exports = function quailBuild(cmd) {
   // Get a list of assessments.
-  config.getLocalConfig(function (data) {
-    state.jquery = data.jquery;
-    state.quail = data.quail;
-    state.assessmentSpecsPath = data.assessmentSpecsPath;
-    quailCore.build();
+  configUtil.getLocalConfig(function (data) {
+    quailCore.build(data);
   });
 };
