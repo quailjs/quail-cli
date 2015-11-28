9730ac333227545b7f01359fd6e4415bb2b1a959
Jesse Renee Beach <splendidnoise@gmail.com>
2015-11-28 12:42:45 -0800

    linted

--- src/quailBuild.js
+++ src/quailBuild.js
@@ -2,14 +2,12 @@
 
 'use strict';
 
-const fs = require('fs');
 const path = require('path');
 const configUtil = require('./config');
 const cwd = process.cwd();
 const quailCore = require(path.join(cwd, 'node_modules', '@quailjs/quail-core'));
-const state = {};
 
-module.exports = function quailBuild(cmd) {
+module.exports = function quailBuild () {
   // Get a list of assessments.
   configUtil.getLocalConfig(function (data) {
     quailCore.build(data);
