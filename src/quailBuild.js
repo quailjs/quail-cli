6dee47bf20ddcbcee0e757725f300ae3ca8f91db
Jesse Renee Beach <splendidnoise@gmail.com>
2015-11-28 22:17:08 -0800

    Move the quail-core require into the build method

--- src/quailBuild.js
+++ src/quailBuild.js
@@ -5,9 +5,11 @@
 const path = require('path');
 const configUtil = require('./config');
 const cwd = process.cwd();
-const quailCore = require(path.join(cwd, 'node_modules', '@quailjs/quail-core'));
 
 module.exports = function quailBuild () {
+  const quailCore = require(
+    path.join(cwd, 'node_modules', '@quailjs/quail-core')
+  );
   // Get a list of assessments.
   configUtil.getLocalConfig(function (data) {
     quailCore.build(data);
